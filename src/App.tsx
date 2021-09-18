d8460c59e1050fc00c27b21037326c969b90e044
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-09-18 12:42:44 +0900

    feat: fix top navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,37 @@
-import './App.module.css';
+import styles from './App.module.css';
 import Home from './Home';
 import Dashboard from './Dashboard';
 
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useLocation
 } from 'react-router-dom';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 function App() {
+  let location = useLocation();
+  console.log('location', location);
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Nav tabs>
+        <NavItem>
+          <NavLink className={styles.linkWrapper} active={window.location.pathname === '/'}><Link className={styles.link} to="/">Home</Link></NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className={styles.linkWrapper} active={window.location.pathname === '/dashboard'}> <Link className={styles.link} to="/dashboard">Dashboard</Link></NavLink>
+        </NavItem>
+      </Nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
