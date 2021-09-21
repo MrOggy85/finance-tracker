f88f95c61705c686d7881ef12933373d632fd137
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2021-09-22 05:56:00 +0900

    fix: font render issue

--- src/core/displayInYen.ts
+++ src/core/displayInYen.ts
@@ -1,5 +1,5 @@
 function displayInYen(amount: number) {
-  return new Intl.NumberFormat('ja-JP', {
+  return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'JPY',
     minimumFractionDigits: 0
